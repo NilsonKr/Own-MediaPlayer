35f9d1b6831c633786a8353c5499e27b46c2ba5e
unknown <nuotrok444@gmail.com>
2021-02-22 20:29:12 -0500

    Preview on the progress bar done

--- src/plugins/updateBar.js
+++ src/plugins/updateBar.js
@@ -1,17 +1,25 @@
 class BarUpdate{
     constructor(){
+        this.preview = document.querySelector('#previewCont')
+        this.previewVideo = document.querySelector('#previewVideo')
         this.barContainer = document.querySelector('#bar-container')
         this.handleUpdate = this.handleUpdate.bind(this)
         this.handleClick = this.handleClick.bind(this)
+        this.handlePreview = this.handlePreview.bind(this)
     }
     run(mediaPlayer){
         this.media = mediaPlayer.media
         this.barTime = mediaPlayer.barTime
 
         this.media.addEventListener('timeupdate', this.handleUpdate)
+        this.barContainer.addEventListener('mousemove', e => this.handlePreview(e))
         this.barContainer.addEventListener('click', e =>  this.handleClick(e))
     }
-
+    handlePreview(ev){
+        const time = (ev.offsetX / this.barContainer.offsetWidth) * this.media.duration
+        this.previewVideo.currentTime = time
+        this.preview.style.left = `calc(${ev.offsetX}px - 50px)`
+    }
     handleClick(ev){
         console.log('ev.offsetX: ' + ev.offsetX)
         console.log('barcontainer.offsetWidth: ' + this.barContainer.offsetWidth)
@@ -21,9 +29,14 @@ class BarUpdate{
     }
     handleUpdate(){
         // console.log(this.barTime.offsetWidth)
-        const time = this.media.currentTime / this.media.duration 
-        this.barTime.style.width = time * 100 + '%' 
+        const time = (this.media.currentTime / this.media.duration) * 100
+        this.barTime.style.width = `${time}%`
+        // this.preview.style.left = `calc(${time}% - 50px)`
+        // this.previewVideo.currentTime = this.media.currentTime
+    }
+    getPosition(){
 
+        return time
     }
 }
 
